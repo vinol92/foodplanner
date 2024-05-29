6743f8120dfef3d514371853c08b445c375a6285
vinol92 <vinol92@gmail.com>
2024-05-29 15:12:00 +0200

    subida

--- app/src/main/java/com/example/foodplanner/Views/MainActivity.java
+++ app/src/main/java/com/example/foodplanner/Views/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         registra.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, Inicio.class);
+                Intent i = new Intent(MainActivity.this, registro.class);
                 startActivity(i);
             }
         });
